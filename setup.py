f50f7e0644a158b8e8bc89cf8bfe8aa731beeb34
maelfa <YOUR_GITHUB_fahras@hotmail.fr>
2020-09-16 00:41:39 +0200

    setup project dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import setuptools
+from distutils.util import convert_path
+
+main_ns = {}
+ver_path = convert_path('c19mlp/__init__.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
 setuptools.setup(
     name='covid_19',
@@ -9,5 +15,5 @@ setuptools.setup(
     install_requires=[
         "scikit-learn==0.22.1",
         "pandas==1.0.1"
-    ]
+    ],
 )
